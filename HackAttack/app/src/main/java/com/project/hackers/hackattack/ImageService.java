d54960107c0c0e96d76c861a9a3d0656cb8db9ce
athatscomp <anilthattayathu@gmail.com>
2015-01-31 04:10:03 -0800

    ImageService

---
+++ HackAttack/app/src/main/java/com/project/hackers/hackattack/ImageService.java
@@ -0,0 +1,38 @@
+package com.project.hackers.hackattack;
+
+/**
+ * Created by athatscomp on 1/31/15.
+ */
+public class ImageService
+{
+    private static ImageService serviceInstance;
+    private static final Object lock = new Object();
+    private ImageService(){
+
+    }
+
+    public static ImageService getServiceInstance()
+    {
+        if(serviceInstance == null)
+        {
+            synchronized (lock)
+            {
+                if (serviceInstance == null)
+                {
+                    serviceInstance = new ImageService();
+                }
+            }
+        }
+        return serviceInstance;
+    }
+
+    public void getImage(){
+
+    }
+    public void createGif(){
+
+    }
+    public void postImage(){
+
+    }
+}
